0141ea75f1a27f62b3e17a8550a084fe7b7a7be7
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-07-20 05:55:21 +0530

    Updates

---
+++ precache-manifest.7e3493f96a1c42f8ed3651a813277ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a1694f1fcf4d7bc112981a43f4ae9f",
+    "url": "/howtocook/index.html"
+  },
+  {
+    "revision": "c6870b639ba956e3df85",
+    "url": "/howtocook/static/css/main.b9d481ea.chunk.css"
+  },
+  {
+    "revision": "b837aea6303b41ba32d9",
+    "url": "/howtocook/static/js/2.1ef5a43b.chunk.js"
+  },
+  {
+    "revision": "213bca7ce55773509172dbee394af2a1",
+    "url": "/howtocook/static/js/2.1ef5a43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6870b639ba956e3df85",
+    "url": "/howtocook/static/js/main.d54d16e5.chunk.js"
+  },
+  {
+    "revision": "9d81a068f90803cb326c",
+    "url": "/howtocook/static/js/runtime-main.419bb934.js"
+  }
+]);
